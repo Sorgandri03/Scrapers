c23cafe6f7ef4529da2a077bb62a1448b0149647
Sorgandri03 <sorgandri03@gmail.com>
2023-10-21 00:16:17 +0200

    aggiunto mercari funzionante, da rifare log e webdriver a parte

---
+++ Mercari.py
@@ -0,0 +1,32 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import *
+
+
+url = 'https://jp.mercari.com/search?keyword=%E6%9C%88%E5%88%8A%E5%B0%91%E5%B9%B4%E3%82%AC%E3%83%B3%E3%82%AC%E3%83%B3%202001'
+
+
+def FindLast(url):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('headless')
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+
+    delay=20
+    try:
+        WebDriverWait(driver, delay).until(expected_conditions.presence_of_element_located((By.XPATH,"//*[@id=\"m74007478429\"]/figure/div[2]")))
+    except TimeoutException:
+        print ("Loading took too much time (first page)!")
+
+    name = driver.find_element(By.XPATH,'//*[@id="m74007478429"]/figure/div[2]').get_attribute("aria-label")
+    price = driver.find_element(By.XPATH,'//*[@id="m74007478429"]/figure/div[3]/div/span/span[2]').text
+    link = driver.find_element(By.XPATH,'//*[@id="item-grid"]/ul/li[1]/div/a').get_attribute("href")
+    print(name,"\n", price,"\n", link)
+    driver.close()
+
+while True:
+    FindLast(url)
